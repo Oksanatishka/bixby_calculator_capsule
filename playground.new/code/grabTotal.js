32c5b94c5c75bcddfb98c4d981c5fed9021eef2f
Oksana <oksanabibik93@gmail.com>
2019-05-28 16:00:07 -0700

    add calculator bixby capsule

---
+++ playground.new/code/grabTotal.js
@@ -0,0 +1,9 @@
+module.exports.function = function grabTotal (firstNumber, secondNumber, operator) {
+  var combine;
+  
+  if(operator == "plus")
+    combine = firstNumber + secondNumber;
+  else
+    combine = -5000;
+  return combine;
+}
